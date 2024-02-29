36f9225fbd6196e503916c9072d66b52a94084df
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-02-29 15:28:56 -0300

    Ini: iniciando o estudo em Python exibindo mensagens.

---
+++ Aula01/boasvondas.py
@@ -0,0 +1,7 @@
+# Vamos criar uma variave para armazenar o
+# nome do usuário
+nome = "admin"
+print("Olá, "+nome+". Seja bem vindo!")
+print("Escreva o seu novo nome de usuário.")
+nome = input("Digite aqui:")
+print("O seu novo usuário é: "+nome)
