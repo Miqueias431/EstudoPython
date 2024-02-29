419dce7067d1236eeb4e0f189be5a9c5aaddc5b5
Miqueias Rodrigues <miqueiasr560@gmail.com>
2024-02-29 17:06:36 -0300

    Feature: Estruturas de desvio de fluxo

---
+++ Aula01/desvioif2.py
@@ -0,0 +1,16 @@
+n1 = input("Digite a primeira nota do aluno: ")
+n2 = input("Digite a segunda nota do aluno:  ")
+n3 = input("Digite a terceira nota do aluno: ")
+n4 = input("Digite a quarta nota do aluno:   ")
+rs = (int(n1)+int(n2)+int(n3)+int(n4)) / 4
+# Se o aluno tiver uma média acima ou igual a 7,
+# então estará APROVADO. Senão se a média
+# for abaixo ou igual a 4, estão estará REPROVADO
+# caso contrário, estará em RECUPERAÇÃO
+print("Sua média é: "+str(rs)+", então você está:" )
+if( rs >= 7 ):
+    print("Aprovado")
+elif( rs <= 4 ):
+    print("Reprovado")
+else:
+    print("Recuperação")
